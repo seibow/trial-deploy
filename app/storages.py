a20d307dc50f525e93d4812612b64a82ba0674e2
seibow <airwhite2525@gmail.com>
2026-01-30 01:36:53 +0900

    storages.py修正

--- app/storages.py
+++ app/storages.py
@@ -1,7 +1,14 @@
 # storages.py
 from storages.backends.s3boto3 import S3Boto3Storage
+from mimetypes import guess_type
 
 class StaticStorage(S3Boto3Storage):
     location = "static"
     default_acl = "public-read"
     querystring_auth = False
+
+    def _save(self, name, content):
+        content_type, _ = guess_type(name)
+        if content_type:
+            content.content_type = content_type
+        return super()._save(name, content)
